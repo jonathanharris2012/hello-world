3b6d6e906292a5acc8d1e37e7de96f368cd0fd8a
Jonathan Harris <jonathan.harris@sonicdrivein.com>
2017-11-17 13:56:01 -0600

    Testing different popup box types

--- Functions.ps1
+++ Functions.ps1
@@ -1,5 +1,5 @@
 function Feedback {
     $dialog = New-Object -ComObject Wscript.Shell
-    
-    $dialog.Popup("Operation Completed",0,"Done",0x6)
+    $dialog.Popup("Attention",0,"Warning",0x30,0x5)
 }
+
